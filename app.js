a24ccd54f852f79fba915f913e702fd2d6b76833
Amirhossein2001 <amirhosseinramezani4455@gmail.com>
2025-04-12 21:47:55 +0330

    mind-grenade.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const sayHi = require("./4-utils");
 const names = require("./3-names");
 const data = require("./5-alternative");
+require("./7-mind-grenade")
 console.log(data)
 sayHi("Ali");
 sayHi(names.john);
