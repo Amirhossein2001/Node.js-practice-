bcc80bdc762610d959c6f8bcc1e85e2c90fc7fd1
Amirhossein2001 <amirhosseinramezani4455@gmail.com>
2025-04-12 21:25:29 +0330

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const sayHi = require("./4-utils");
+const names = require("./3-names");
+const data = require("./5-alternative");
+console.log(data)
+sayHi("Ali");
+sayHi(names.john);
+sayHi(names.peter);
